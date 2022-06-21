b6b695d06f6c77aea65126119eae0fed37ed6d59
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-21 17:53:37 +0530

    commit

---
+++ src/ProgramPractice/userInput.java
@@ -0,0 +1,17 @@
+package ProgramPractice;
+
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+
+        int x;
+        int y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("the sum of two above numbers is ="+(x + y));
+    }
+
+
+}
