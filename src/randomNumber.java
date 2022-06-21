bdce1a45c4c5929337a2880c236c84ff2c7a8cbd
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-21 17:45:42 +0530

    Initail commit

---
+++ src/randomNumber.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class randomNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println("values are" + x);
+
+    }
+}
